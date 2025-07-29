d911bf7efd20859e4dee1630a56d27b0d4faca8e
Janisar <janisarhasnain@gmail.com>
2025-07-30 00:34:02 +0500

    python classes/objects

---
+++ Syntax/p_basics_5.py
@@ -0,0 +1,41 @@
+# OOP 
+
+# Classes & object
+
+# car data 
+
+car = {
+    "name": "janisar"
+}
+
+# class
+class Car:
+    def __init__(self, brand, model, is_New, ownerShip):
+        self.brand = brand
+        self.model = model
+        self.is_New = is_New
+        self.ownerShip = ownerShip
+    # def __str__(self):
+    #     return f"{self.is_New}"
+    def salmanCar(self):
+        return f"Salman purchased this {self.brand} car!"
+
+
+# object
+car1 = Car("mehran",2019,False,3)
+car1 = Car("mehran",2019,False,3)
+car1 = Car("mehran",2019,False,3)
+car1 = Car("mehran",2019,False,3)
+car1 = Car("mehran",2019,False,3)
+car1 = Car("mehran",2019,False,3)
+car1 = Car("mehran",2019,False,3)
+car1 = Car("mehran",2019,False,3)
+car1 = Car("mehran",2019,False,3)
+car1 = Car("mehran",2019,False,3)
+car1 = Car("mehran",2019,False,3)
+car1 = Car("mehran",2019,False,3)
+car1 = Car("mehran",2019,False,3)
+car1 = Car("mehran",2019,False,3)
+car1 = Car("mehran",2019,False,3)
+print("Car 1: ",car1.salmanCar())
+
