64bd4dfaf47098030623e72225e798ac56517e6d
Janisar <janisarhasnain@gmail.com>
2025-08-03 00:54:57 +0500

    NumPy basic operations

--- NumPy Basics/numpy01.py
+++ NumPy Basics/numpy01.py
@@ -25,9 +25,33 @@ import numpy as np
 
 
 
-# Some usefull methods of NumPy
-print(np.zeros((2, 3)))         # 2x3 matrix of 0s
-print(np.ones((3, 3)))          # 3x3 matrix of 1s
-print(np.arange(0, 12, 2))      # [0 2 4 6 8]
-print(np.linspace(0, 1, 5))     # 5 values from 0 to 1
-print(np.random.rand(2, 2))     # 2x2 matrix with random values
+# # Some usefull methods of NumPy
+# print(np.zeros((2, 3)))         # 2x3 matrix of 0s
+# print(np.ones((3, 3)))          # 3x3 matrix of 1s
+# print(np.arange(0, 12, 2))      # [0 2 4 6 8]
+# print(np.linspace(0, 1, 5))     # 5 values from 0 to 1
+# print(np.random.rand(2, 2))     # 2x2 matrix with random values
+
+
+
+
+# # NumPy Array operations
+# a = np.array([1, 2, 3])
+# b = np.array([4, 5, 6])
+
+# print("Add:", a + b)
+# print("Multiply:", a * b)
+# print("Mean:", np.mean(a))
+# print("Dot Product:", np.dot(a, b))
+
+
+
+# # reshape and flatten
+# arr = np.array([[1, 2, 3], [4, 5, 6]])
+# reshaped = arr.reshape((3, 2))
+# print("Reshaped:\n", reshaped)
+
+# flat = arr.flatten()
+# print("Flattened:", flat)
+
+
